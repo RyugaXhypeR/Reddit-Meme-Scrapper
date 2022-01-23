4b921cdae06da42e6d2776c1e767984708acee4b
RyugaXhypeR <91488988+RyugaXhypeR@users.noreply.github.com>
2022-01-23 18:14:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ for i in range(NO_OF_MEMES):
     title = submission[i].title
     
     for j in title:
-        if j in INVALID_SYM:
+        if j.lower() not in VALID_SYM:
             title = title.replace(j, '-')    
     
     #if image_check(url) == True:      --> you can enable this you dont want to download .gif
