f99255637bccf64d6e28a2d95121bac502f0dc62
RyugaXhypeR <91488988+RyugaXhypeR@users.noreply.github.com>
2022-01-23 18:14:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ folder_name = #Name of the folder you want to store your memes
 SUBREDDIT_NAME = #Name of the subreddit from which you want to get memes
 NO_OF_MEMES = #No of memes.
 LIMIT = #The amount of memes do you want to select from
+VALID_SYM = 'abcdefghijklmnopqrstuvwxyz'
 
 def image_check(link):
     """
@@ -20,13 +21,11 @@ def image_check(link):
 
 # create an application here - https://www.reddit.com/prefs/apps to get access to this
 reddit = praw.Reddit(
-            client_id = "",
-            client_secret = "",
-            username = "", 
-            user_agent = "",
+            client_id = '',
+            client_secret = '',
+            username = '', 
+            user_agent = '',
 )
-
-INVALID_SYM = (",", ".", "'", "*", "?", "/", "_", '"', "!")
 
 subreddit = reddit.subreddit(SUBREDDIT_NAME)
 lim = subreddit.top(limit=LIMIT)
@@ -40,10 +39,10 @@ for i in range(NO_OF_MEMES):
     
     for j in title:
         if j in INVALID_SYM:
-            title = title.replace(j, "-")    
+            title = title.replace(j, '-')    
     
     #if image_check(url) == True:      --> you can enable this you dont want to download .gif
-    print(f"{i+1}. {url}")
+    print(f'{i+1}. {url}')
     r = requests.get(url).content
-    with open(f"{folder_name}/{title}.jpg", "wb+") as f:
+    with open(f"{folder_name}/{title}.jpg", 'wb+') as f:
         f.write(r)
